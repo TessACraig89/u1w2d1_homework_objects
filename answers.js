09884d2cae17aa6a027d56fe2e2feed6ed8135b2
Tess Craig <tessashleycraig89@gmail.com>
2017-12-11 18:22:21 -0600

    Commit 2 - Datatypes answers

---
+++ answers.js
@@ -0,0 +1,149 @@
+// Datatypes
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// 1.A light switch that can be either on or off.
+Boolean
+const isLightSwitchOn === true;
+
+while (isLightSwitchOn === true) {
+    console.log('Light is on.');
+};
+
+// 2.A user's email address.
+String
+const userEmail = "tessashleycraig89@gmail.com";
+
+// 3.A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+Object
+const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"];
+
+// 4.A list of student names from our class.
+Object
+const classNames = ["Lillian", "Jake", "Christopher", "Khoa", "Marcy", "Max", "Neo", "Scott", "Tess"];
+
+// 5.A list of student names from our class, each with a location.
+Object
+const class = {
+  { name: "Lillian", location: "Austin" },
+  { name: "Jake", location: "Austin" },
+  { name: "Christopher", location: "Austin" },
+  { name: "Khoa", location: "Austin" },
+  { name: "Marcy", location: "Austin" },
+  { name: "Max", location: "Austin" },
+  { name: "Neo", location: "Austin" },
+  { name: "Scott", location: "Austin" },
+  { name: "Tess", location: "Austin" },
+};
+
+// 6.A list of student names from our class, each with a location and each with a list of favorite tv shows.
+Object
+const class = [
+  { name: "Tess", location: "Austin", favoriteShows: ["Star Trek Voyager", "Game of Thrones"] },
+  { name: "Lillian", location: "Austin", favoriteShows: ["Medieval Cooking Show"] }
+];
+
+
+// Take it Easy
+// 1.Make an array that holds all of the colors of the rainbow.
+
+// 2.Write code that will access "blue" from the rainbow array.
+
+// 3.Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+// 4.Write code that will access your hobby from the object that you just created.
+
+
+//Crazy Object
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+//Use crazy Object to log the following.
+
+// 1."omg my mouth is burning"
+
+// 2."Pretty pretty prettayyyyy good"
+
+// 3."Swearing at Larry and Jeff"
+
+// 4."Chicken Teriyaki Boyyyyyy"
+
+// 5.The object the contains the name funkhauser
+
+
+// Object-ception
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+//Change the value of limbo to null.
+
+
+// Bond Films
+// Array of Objects:
+const bondFilms = [
+  { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+  { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+  { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+  { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+  { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+  { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+  { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+  { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+  { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+  { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+  { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+  { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+  { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+  { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+  { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+  { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+  { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+  { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+  { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+  { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+  { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+  { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+  { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+];
+//Use for loops and conditionals and methods in order to complete the below:
+
+// 1.Create a new array called bondTitles with only the titles of the Bond films, and console.log the new array.
+
+// 2. Create a new array oddBonds, of only the Bond films released on odd-numbered years.
+
+// 3. Determine the total cumulative gross of the Bond franchise, and console.log the result. hint To make the grosses into usable numbers, look into the .replace Javascript method (there are many ways to do this, however). Look into parseInt also.
